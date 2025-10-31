622e1bf5e3952a436a0049a91344fec11bae5830
garzandoy <garzandoy@gmail.com>
2025-10-30 21:28:59 -0400

    Remove FAQ

--- app/page.js
+++ app/page.js
@@ -1,7 +1,6 @@
 // app/page.js -- This is the landing page. 
 import Link from "next/link";
 import { Users, Star, Award } from "lucide-react";
-// import FAQ from "./components/FAQ";
 
 
 export const metadata = {
